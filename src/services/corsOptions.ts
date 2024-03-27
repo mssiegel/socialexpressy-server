846c930840060e5af1f77edbccd391200fed1079
Moshe Siegel <siegel.moshes@gmail.com>
2024-03-27 21:41:29 +0200

    Update corsOptions.ts

--- src/services/corsOptions.ts
+++ src/services/corsOptions.ts
@@ -1,6 +1,6 @@
 // Allowed client side URLs
 const LOCAL_URL = 'http://localhost:3000';
-const PRODUCTION_URL = 'https://socialexpressy-client.vercel.app/';
+const PRODUCTION_URL = 'https://socialexpressy.vercel.app/';
 const VERCEL_DEPLOY_PREVIEW_URL = /https:..socialexpressy.*vercel.app/;
 
 const corsOptions = {
