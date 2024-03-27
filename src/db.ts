a56a0034e1ef51cf77f74def80fbdef3d2c73451
Moshe Siegel <siegel.moshes@gmail.com>
2024-03-27 20:58:44 +0200

    Added settings for production database

--- src/db.ts
+++ src/db.ts
@@ -2,14 +2,30 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db_name = 'socialexpressy';
-const db_username = 'root';
-const db_password = process.env.DB_PASSWORD;
-const local_host = '127.0.0.1';
+let db_name: string;
+let db_username: string;
+let db_password: string;
+let db_host: string;
+let db_port: number;
+if (process.env.NODE_ENV === 'development') {
+  db_name = 'socialexpressy';
+  db_username = 'root';
+  db_password = process.env.DB_DEV_PASSWORD as string;
+  db_host = '127.0.0.1'; // local host
+  db_port = 3306;
+} else {
+  db_name = 'defaultdb';
+  db_username = 'avnadmin';
+  db_password = process.env.DB_PROD_PASSWORD as string;
+  db_host = 'socialexpressy-socialexpressy.a.aivencloud.com';
+  db_port = 18739;
+}
 
 const sequelize = new Sequelize(db_name, db_username, db_password, {
-  host: local_host,
+  host: db_host,
   dialect: 'mysql',
+  port: db_port,
+  ssl: true,
 });
 
 try {
