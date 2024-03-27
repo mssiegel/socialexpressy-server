027161a6ac3e3e3c262cc26a58e2d743c0fc5af0
Moshe Siegel <siegel.moshes@gmail.com>
2024-03-27 21:24:01 +0200

    Added procfile for heroku

--- src/services/corsOptions.ts
+++ src/services/corsOptions.ts
@@ -1,8 +1,10 @@
 // Allowed client side URLs
 const LOCAL_URL = 'http://localhost:3000';
+const PRODUCTION_URL = 'https://socialexpressy-client.vercel.app/';
+const VERCEL_DEPLOY_PREVIEW_URL = /https:..socialexpressy.*vercel.app/;
 
 const corsOptions = {
-  origin: [LOCAL_URL],
+  origin: [LOCAL_URL, PRODUCTION_URL, VERCEL_DEPLOY_PREVIEW_URL],
 };
 
 export default corsOptions;
