ed25612add16b0dc8aea1675eadea4338775b000
Moshe Siegel <siegel.moshes@gmail.com>
2024-03-31 22:59:14 +0300

    Added logging using Morgan

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import 'express-async-errors';
+import morgan from 'morgan';
 
 import corsOptions from './services/corsOptions.js';
 import userRoutes from './user.routes.js';
@@ -11,6 +12,12 @@ import { errorHandler } from './middlewares/errors.js';
 const app = express();
 app.use(express.json());
 app.use(helmet());
+
+// logging
+const morganSetting =
+  process.env.NODE_ENV === 'development' ? 'tiny' : 'combined';
+app.use(morgan(morganSetting));
+
 app.use(cors(corsOptions));
 
 app.use('/api/v1/users', userRoutes);
